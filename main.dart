fad4f3c84e88451e9d6d95bd14d826ededa3981b
fannizati <fannizati.fzh@gmail.com>
2019-07-01 14:39:08 +0700

    first commit

---
+++ main.dart
@@ -0,0 +1,19 @@
+void main(){
+  String name = "teteng";
+  int age = 23;
+  bool status =true;
+  List<String> kelas = ['papan', 'Penghapus'];
+List umurSiswa = [14, 15, 16, 'String', true];
+Map Siswa = {'nama':"nama" };
+
+
+// print(message1);
+
+String name2 = 'Gatot';
+
+
+// print(message2);
+
+int a = 29;
+int b = 10;
+}
